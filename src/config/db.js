250aedc9e0eedab3e59ece6d7d309dbd3156b5e5
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-07-14 17:51:40 -0500

    otro deploy 1

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,23 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize("finTrack", "postgres", "root", {
   host: "localhost",
   dialect: "postgres",
+  port: 5432,
 });
+
+const connectDB = async () => {
+  try {
+    // Intentar conectar con la base de datos
+    await sequelize.authenticate();
+    console.log("✅ Conexión a PostgreSQL establecida correctamente");
+
+    // Sincronizar la base de datos
+    await sequelize.sync({ alter: true });
+  } catch (error) {
+    console.error("❌ No se pudo conectar a PostgreSQL:", error.message);
+    process.exit(1); // Si no puede conectar, finalizar el proceso
+  }
+};
+
+connectDB();
 
 export default sequelize;
